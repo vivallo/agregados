dbd13cb3d6e1ee9b49577b5ed6ff31138483f714
vivallo <86535902+vivallo@users.noreply.github.com>
2024-05-24 22:22:32 -0400

    Update posttype-funciones-ed.php

--- wordpress/pluginsED/posttype-funciones-ed.php
+++ wordpress/pluginsED/posttype-funciones-ed.php
@@ -1,1 +1,14 @@
+<?php
+/*
+Plugin Name: Post Type y Funciones CyR
+Plugin URI: https://webgrafica.cl/
+Description: Este plugin es para crear los post type y funciones de CyR Extintores 
+Author: Web Grafica
+Author URI: https://webgrafica.cl/
+Version: 1.0.0
+*/
 
+require_once plugin_dir_path(__FILE__) . 'includes/ptCyR-functions.php';
+require_once plugin_dir_path(__FILE__) . 'includes/opcionesCyR-functions.php';
+require_once plugin_dir_path(__FILE__) . 'includes/metaboxCyR-functions.php';
+?>
