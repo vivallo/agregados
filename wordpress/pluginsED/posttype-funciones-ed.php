b4fe58feb58af6a0f64f2f7e149b0bef02d57a8a
vivallo <86535902+vivallo@users.noreply.github.com>
2024-05-24 22:30:11 -0400

    Actualizado

--- wordpress/pluginsED/posttype-funciones-ed.php
+++ wordpress/pluginsED/posttype-funciones-ed.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Post Type y Funciones ED
 Plugin URI: https://github.com/vivallo/
-Description: Este plugin es para crear los post type y funciones de CyR Extintores 
+Description: Este plugin es para crear los post-type y funciones de Extintores a Domicilio
 Author: Web Grafica
 Author URI: https://webgrafica.cl/
 Version: 1.0.0
